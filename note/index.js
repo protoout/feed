a71302a454cb0efc76ef7bfa9768cc8ec60e771a
n0bisuke <rsksound@gmail.com>
2022-02-21 22:49:39 +0900

    update discord.js

--- note/index.js
+++ note/index.js
@@ -2,7 +2,7 @@
 const fs = require(`fs`);
 const getMagazine = require(`./getMagazine`);
 
-const MAGAZINE_KEY_INTERVIEW = `m27cd63996372`; //プロトアウトインタビューマガジン
+const MAGAZINE_KEY_INTERVIEW = `m172a343aee88`; //プロトアウトインタビューマガジン
 const MAGAZINE_KEY_ZENTAI = `m03f109eaf0a8`; //プロトアウト学生全体マガジン
 
 (async ()=>{
